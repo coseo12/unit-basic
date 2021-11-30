381291e6abfd7aea94128b63b198e2c58ac17bc4
coseo12 <coseo1221@gmail.com>
2021-11-30 15:13:02 +0900

    add Stub

--- src/mock/product/__test__/product_service_no_di.test.js
+++ src/mock/product/__test__/product_service_no_di.test.js
@@ -1,20 +1,11 @@
 const ProductService = require('../product_service_no_di.js');
-const ProductClient = require('../product_client.js');
+const StubProductClient = require('./stub_product_client.js');
 
-jest.mock('../product_client');
-
-describe('ProductService', () => {
-  const fetchItems = jest.fn(async () => [
-    { item: '🥛', available: true },
-    { item: '🍌', available: false },
-  ]);
-
-  ProductClient.mockImplementation(() => ({ fetchItems }));
-
+describe('ProductService - Stub', () => {
   let productService;
 
   beforeEach(() => {
-    productService = new ProductService();
+    productService = new ProductService(new StubProductClient());
   });
 
   it('should filter out only available items', async () => {
@@ -23,3 +14,26 @@ describe('ProductService', () => {
     expect(items).toEqual([{ item: '🥛', available: true }]);
   });
 });
+
+// jest.mock('../product_client');
+
+// describe('ProductService', () => {
+//   const fetchItems = jest.fn(async () => [
+//     { item: '🥛', available: true },
+//     { item: '🍌', available: false },
+//   ]);
+
+//   ProductClient.mockImplementation(() => ({ fetchItems }));
+
+//   let productService;
+
+//   beforeEach(() => {
+//     productService = new ProductService();
+//   });
+
+//   it('should filter out only available items', async () => {
+//     const items = await productService.fetchAvailableItems();
+//     expect(items.length).toBe(1);
+//     expect(items).toEqual([{ item: '🥛', available: true }]);
+//   });
+// });
