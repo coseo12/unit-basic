7c01ca992fe66314628659631db86557be281e98
coseo12 <coseo1221@gmail.com>
2021-11-30 14:31:37 +0900

    add check

--- src/mock/check/__test__/check.test.js
+++ src/mock/check/__test__/check.test.js
@@ -12,6 +12,16 @@ describe('check', () => {
   it('should call onSuccess when predicate is true', () => {
     check(() => true, onSuccess, onFail);
 
-    expect(onSuccess.mock.calls.length).toBe(1);
+    expect(onSuccess).toHaveBeenCalledTimes(1);
+    expect(onSuccess).toHaveBeenCalledWith('yes');
+    expect(onFail).toHaveBeenCalledTimes(0);
+  });
+
+  it('should call onFail when predicate is false', () => {
+    check(() => false, onSuccess, onFail);
+
+    expect(onFail).toHaveBeenCalledTimes(1);
+    expect(onFail).toHaveBeenCalledWith('no');
+    expect(onSuccess).toHaveBeenCalledTimes(0);
   });
 });
